c08a63f6c153f71fdc35398c437d904ff16cf77b
Wilfred Tse <skyline2016@gmail.com>
2017-01-24 22:16:23 +0800

    reverse the wrong changes

--- application/views/login.php
+++ application/views/login.php
@@ -5,40 +5,40 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title" id="login_modal">Login</h4>
+        <h4 class="modal-title" id="login_modal"><?php echo lang('login')?></h4>
       </div>
       <div class="modal-body">
     <div class="row">
         <div class="col-xs-6">
             <div class="well">
-                <form id="login-warning-form" action="/login/login" method="POST">
+                <form id="login-form" action="/login/login" method="POST">
                     <div class="row">
                         <div class="col-md-12">
                               <div class="form-group">
-                                  <label for="username" class="control-label">Username/E-mail</label>
+                                  <label for="username" class="control-label"><?php echo lang('username')?></label>
                                   <input type="text" class="form-control" name="username" value="" required title="Username/E-mail" placeholder="Username/E-mail" required>
                                   <span class="help-block"></span>
                               </div>
                               <div class="form-group">
-                                  <label for="password" class="control-label">password <a data-toggle="modal" data-target="#forgot_password_warning" style="cursor:pointer;">(forgot password)</a></label>
-                                  <input type="password" class="form-control" name="password" placeholder="password" value="" required="" title="password" required>
+                                  <label for="password" class="control-label"><?php echo lang('password')?><a data-toggle="modal" data-target="#forgot_password" style="cursor:pointer;"><?php echo lang('forget_password')?></a></label>
+                                  <input type="password" class="form-control" name="password" placeholder="<?php echo lang('password')?>" value="" required="" title="password" required>
                                   <span class="help-block"></span>
                               </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <input id="submit" type="submit" value="Login" class="btn btn-success btn-block" />
+                            <input id="submit" type="submit" value="<?php echo lang('login')?>" class="btn btn-success btn-block" />
                         </div>
                         <div class="col-md-6">
-                            <a href="/register">Register</a>
+                            <a href="/register"><?php echo lang('register')?></a>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
           <div class="col-xs-6">
-              <h5>Login with social network account</h5>
+              <h5><?php echo lang('other_login')?></h5>
                 <ul class="list-unstyled list-social">
                     <li>
                         <a href="#" class="btn btn-lg btn-primary btn-block">
@@ -67,7 +67,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Forgot password</h4>
+        <h4 class="modal-title"><?php echo lang('forgot_password')?></h4>
       </div>
       <div class="modal-body">
         <form id="forgot-password-warning-form" action="/login/forgot_password" method="post">
@@ -105,10 +105,10 @@
                 type: "POST",
                 data: $("#login-warning-form").serialize(),
                 success: function(data){
-                    window.open(data,"_self")
+                    window.open("/design_sticker","_self")
                 }
             });
        });
     });
 </script>
-<center><font color="red">Please login before using this function!</font><center>
+<center><font color="red"><?php echo lang('login_warning')?></font><center>
